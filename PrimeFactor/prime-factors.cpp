36d9ca8f2731d486e94ab547e7ad2cce14cc1070
INHO <choo4r@gmail.com>
2025-07-02 10:39:59 +0900

    [refactor] Of12

--- PrimeFactor/prime-factors.cpp
+++ PrimeFactor/prime-factors.cpp
@@ -6,21 +6,14 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
-			int devisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12) {
-				for (devisor = 2; number > 1; devisor++) {
-					while (number % devisor == 0) {
-						result.push_back(devisor);
-						number /= devisor;
-					}
-				}
+
+		for (int devisor = 2; number > 1; devisor++) {
+			while (number % devisor == 0) {
+				result.push_back(devisor);
+				number /= devisor;
 			}
-			else {
-				result.push_back(number);
-			}
-			
 		}
+		
 		return result;
 	}
 };
